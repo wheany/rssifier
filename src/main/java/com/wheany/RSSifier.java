061908ade0a9f1fa7bfdcc0582c51c1d754e0c55
Jan-Erik Finnberg <wheany@iki.fi>
2015-10-04 19:28:21 +0300

    Add main class.

---
+++ src/main/java/com/wheany/RSSifier.java
@@ -0,0 +1,12 @@
+package com.wheany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RSSifier {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RSSifier.class, args);
+    }
+}
