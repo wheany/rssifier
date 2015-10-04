a5b0ea0f1540824a5ad8aa4e544606724f43fc5c
Jan-Erik Finnberg <wheany@iki.fi>
2015-10-04 13:13:28 +0300

    Rip off UI from Vaadin Spring tutorial

---
+++ src/main/java/com/wheany/BaseUI.java
@@ -0,0 +1,37 @@
+package com.wheany;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.navigator.SpringViewProvider;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Theme("valo")
+@SpringUI
+public class BaseUI extends UI {
+
+    // we can use either constructor autowiring or field autowiring
+    @Autowired
+    private SpringViewProvider viewProvider;
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        root.setMargin(true);
+        root.setSpacing(true);
+        setContent(root);
+
+        final Panel viewContainer = new Panel();
+        viewContainer.setSizeFull();
+        root.addComponent(viewContainer);
+        root.setExpandRatio(viewContainer, 1.0f);
+
+        Navigator navigator = new Navigator(this, viewContainer);
+        navigator.addProvider(viewProvider);
+    }
+}
