106b5f43a508fb8b9a8029710d0e9cf20ec92270
Jan-Erik Finnberg <wheany@iki.fi>
2015-10-04 20:52:00 +0300

    Change text fields to preformatted labels in the mockup.

--- src/main/java/com/wheany/DefaultView.java
+++ src/main/java/com/wheany/DefaultView.java
@@ -2,6 +2,8 @@ package com.wheany;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.AbstractErrorMessage;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.*;
 
@@ -33,13 +35,10 @@ public class DefaultView extends VerticalLayout implements View {
         selectorBar.setComponentAlignment(selectorTestButton, Alignment.BOTTOM_LEFT);
         addComponent(selectorBar);
 
-        TextArea itemPreview = new TextArea();
-        itemPreview.setValue("<li class=\"news-item\">\n" +
+        Label itemPreview = new Label("<li class=\"news-item\">\n" +
                 "\t<a class=\"news-link\" href=\"http://example.com/news-item1.html\">This is an interesting link</a>\n" +
                 "\t<a class=\"unrelated-link\" href=\"http://example.com/pictures-of-kittens\">This is an unrelated link</a>\n" +
-                "</li>");
-        itemPreview.setReadOnly(true);
-        itemPreview.setWidth(50, Unit.EM);
+                "</li>", ContentMode.PREFORMATTED);
         addComponent(itemPreview);
 
         HorizontalLayout linkSelectorBar = new HorizontalLayout();
@@ -52,10 +51,7 @@ public class DefaultView extends VerticalLayout implements View {
         linkSelectorBar.setComponentAlignment(linkSelectorTestButton, Alignment.BOTTOM_LEFT);
         addComponent(linkSelectorBar);
 
-        TextField urlPreview = new TextField();
-        urlPreview.setValue("http://example.com/news-item1.html");
-        urlPreview.setReadOnly(true);
-        urlPreview.setWidth(50, Unit.EM);
+        Label urlPreview = new Label("http://example.com/news-item1.html", ContentMode.PREFORMATTED);
         addComponent(urlPreview);
 
         HorizontalLayout nextPageSelectorBar = new HorizontalLayout();
@@ -69,10 +65,7 @@ public class DefaultView extends VerticalLayout implements View {
 
         addComponent(nextPageSelectorBar);
 
-        TextField nextPagePreview = new TextField();
-        nextPagePreview.setValue("<a class=\"next-page\" href=\"news-page2.html\">");
-        nextPagePreview.setReadOnly(true);
-        nextPagePreview.setWidth(50, Unit.EM);
+        Label nextPagePreview = new Label("<a class=\"next-page\" href=\"news-page2.html\">", ContentMode.PREFORMATTED);
         addComponent(nextPagePreview);
     }
 
