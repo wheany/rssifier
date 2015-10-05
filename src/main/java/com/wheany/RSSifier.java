42e0288d0948ef53a630c3bc8dd36af8e98ee4f7
Jan-Erik Finnberg <wheany@iki.fi>
2015-10-05 18:47:39 +0300

    Remove test code from main

--- src/main/java/com/wheany/RSSifier.java
+++ src/main/java/com/wheany/RSSifier.java
@@ -19,60 +19,6 @@ public class RSSifier {
     public static final String DOWNLOAD_TEST_URL = "https://wheany.com/test/";
 
     public static void main(String[] args) {
-        JAXBContext jaxbContext;
-        Unmarshaller jaxbUnmarshaller;
-        Rss rss;
-        try {
-            jaxbContext = JAXBContext.newInstance(Rss.class);
-            jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            rss = (Rss) jaxbUnmarshaller.unmarshal(RSSifier.class.getResourceAsStream("/Liftoff News.xml"));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            rss = new Rss();
-        }
-
-        rss.getChannel().getItemOrTitleOrLinkOrDescriptionOrLanguageOrCopyrightOrManagingEditorOrWebMasterOrPubDateOrLastBuildDateOrCategoryOrGeneratorOrDocsOrCloudOrTtlOrImageOrTextInputOrSkipHoursOrSkipDays()
-                .stream()
-                .filter(channelItem -> channelItem instanceof Item)
-                .forEach(channelItem -> {
-                    Item item = (Item) channelItem;
-                    for (Object titleOrDescription : item.getTitleOrDescription()) {
-                        if (titleOrDescription instanceof Title) {
-                            System.out.println("Title:" + ((Title) titleOrDescription).getvalue());
-                        }
-                        if (titleOrDescription instanceof Description) {
-                            System.out.println("Description:" + ((Description) titleOrDescription).getvalue());
-                        }
-                    }
-
-                    for (Object subItem : item.getAuthorOrCategoryOrCommentsOrEnclosureOrGuidOrPubDateOrSource()) {
-                        if (subItem instanceof Guid) {
-                            System.out.println("Guid:" + ((Guid) subItem).getvalue());
-                        }
-                        if (subItem instanceof Author) {
-                            System.out.println("Author:" + ((Author) subItem).getvalue());
-                        }
-                    }
-                    if (item.getLink() != null) {
-                        System.out.println("Link:" + item.getLink().getvalue());
-                    }
-                });
-        try {
-            Downloader downloader = new Downloader(DOWNLOAD_TEST_URL);
-
-            Document document = Jsoup.parse(downloader.download().toFile(), null, DOWNLOAD_TEST_URL);
-
-            Elements elements = document.select("a[href]");
-
-            elements.stream().forEach(element -> System.out.println("Element:" + element.outerHtml()));
-
-        } catch (MalformedURLException e) {
-            System.err.println("Malformed URL");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.err.println("IO exception");
-            e.printStackTrace();
-        }
         SpringApplication.run(RSSifier.class, args);
     }
 }
